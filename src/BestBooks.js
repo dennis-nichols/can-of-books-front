7f755f626ed5b33389309b7abef5eec11673fd23
Dennis Nichols <dgnichols22@gmail.com>
2022-10-31 19:03:00 -0300

    Add carousel part 2

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React from 'react';
+import BooksCarousel from './BooksCarousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,6 +12,25 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  getBooks = async() => {
+    try {
+      let bookData = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+      this.setState({
+        books: bookData.data
+      })
+      console.log('We have books!');
+      console.log(this.state.books);
+    }
+    catch (error) {
+      console.log('Books not retrieved due to: ', error.reponse);
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+  }
+
+
   render() {
 
     /* TODO: render all the books in a Carousel */
@@ -19,7 +40,9 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <BooksCarousel 
+          books = {this.state.books}>
+          </BooksCarousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
