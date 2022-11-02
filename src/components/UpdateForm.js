1f533df541a5f3394c9c842bf8657220ad866884
Dennis Nichols <dgnichols22@gmail.com>
2022-11-02 16:43:37 -0300

    fixed bug with leftover component

--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import ToggleButton from 'react-bootstrap/ToggleButton';
 
 class UpdateForm extends React.Component {
   render() {
